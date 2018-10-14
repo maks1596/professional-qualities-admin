fde76fe4a5993ff3324b8597f9037018b8d41c98
Максим Шестопёров <maks1596@mail.ru>
2018-10-14 15:45:39 +0400

    Установлен INavigation для NavigationView

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "Entities/User/User.h"
 #include "Modules/Autorization/View/LoginForm.h"
 #include "MainWindow/MainWindow.h"
+#include "Navigation/NavigationView.h"
 
 int main(int argc,  char **argv) {
     QStringList paths = QCoreApplication::libraryPaths();
@@ -19,6 +20,7 @@ int main(int argc,  char **argv) {
     QApplication a(argc, argv);
     Configurator::readConfigurations();
     MainWindow mainWindow;
+    NavigationView::setNavigation(&mainWindow);
 
     LoginForm loginForm;
     loginForm.show();
