50dac4f40463093e639ebe086165adea71dc0b40
Максим Шестопёров <maks1596@mail.ru>
2018-10-14 15:50:57 +0400

    Изменена видимость push у NavigationView

--- Navigation/NavigationView.cpp
+++ Navigation/NavigationView.cpp
@@ -11,23 +11,27 @@ INavigation *NavigationView::m_navigation = nullptr;
 //  :: Lifecycle ::
 
 NavigationView::NavigationView(QWidget *parent)
-    : QWidget(parent)
-{ }
+    : QWidget(parent) { }
 
-//   :: Public methods ::
+//  :: Public methods ::
 
-void NavigationView::push(QWidget *widget) {
-    assert(m_navigation != nullptr);
-    m_navigation->push(widget);
-}
 void NavigationView::pop() {
     assert(m_navigation != nullptr);
     m_navigation->pop();
 }
+
+//  :: Static public methods ::
 
 INavigation *NavigationView::getNavigation() {
     return m_navigation;
 }
 void NavigationView::setNavigation(INavigation *navigation) {
     m_navigation = navigation;
+}
+
+//  :: Protected methdos ::
+
+void NavigationView::push(QWidget *widget) {
+    assert(m_navigation != nullptr);
+    m_navigation->push(widget);
 }
--- Navigation/NavigationView.h
+++ Navigation/NavigationView.h
@@ -8,11 +8,13 @@ class NavigationView : public QWidget {
 public:
     explicit NavigationView(QWidget *parent = nullptr);
 
-    void push(QWidget *widget);
     void pop();
 
     static INavigation *getNavigation();
     static void setNavigation(INavigation *navigation);
+
+protected:
+    void push(QWidget *widget);
 
 private:
     static INavigation *m_navigation;
